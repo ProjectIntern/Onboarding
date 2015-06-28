09a38a383a0d05cce308c4c1158b3297599b70b2
David Li <david_li@gap.com>
2015-06-28 00:44:16 -0700

    Updated login, maybe buggy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get '/login' => 'login#index' 
+  get 'signup' => 'users#new'
+  resources :users
+
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
