69f4d63cbcd2ad35f976e86428ed10ed711eb0f8
David Li <david_li@gap.com>
2015-06-29 22:20:54 -0700

    Implemented welcome page and cleaned up the design of the application.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   resources :users
   get '/exists' => 'users#index'
-
   get '/login' => 'sessions#new'
   post 'login' => 'sessions#create'
   get '/error' => 'sessions#index'
