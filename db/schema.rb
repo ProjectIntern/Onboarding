73c4c652e0ee2da82549075f4bda5103b0444eac
David Li <david_li@gap.com>
2015-07-07 13:59:50 -0700

    Added seed data for testing

--- db/schema.rb
+++ db/schema.rb
@@ -11,25 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706041148) do
+ActiveRecord::Schema.define(version: 20150702195157) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "profiles", force: :cascade do |t|
-    t.string   "image"
-    t.string   "facebook"
-    t.string   "linkedin"
-    t.string   "twitter"
-    t.string   "instagram"
-    t.string   "location"
-    t.string   "position"
-    t.string   "school"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,10 +30,11 @@ ActiveRecord::Schema.define(version: 20150706041148) do
     t.string   "linkedin",        default: ""
     t.string   "twitter",         default: ""
     t.string   "instagram",       default: ""
-    t.string   "location",        default: "location"
-    t.string   "position",        default: "position"
-    t.string   "school",          default: "education"
-    t.string   "about",           default: "I love GAP"
+    t.string   "location"
+    t.string   "default"
+    t.string   "position"
+    t.string   "school"
+    t.string   "about"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
