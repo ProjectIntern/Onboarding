715f480aebcced8eed9ccd1bdaf738b44a903ed3
Christie Yeh <yehchristie@gmail.com>
2015-08-11 11:33:47 -0700

    fixed seeding problem. downloaded imagemagick

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,17 @@ ActiveRecord::Schema.define(version: 20150803170616) do
 
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
+  create_table "globals", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "image"
+    t.string   "position"
+    t.string   "about"
+  end
+
   create_table "hire_type_boards", force: :cascade do |t|
     t.string   "name"
     t.string   "banner"
