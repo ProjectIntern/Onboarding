b2f1b3da9de1ce3bdf086ed680c59afe6dae3ff1
David Li <david_li@gap.com>
2015-08-12 20:21:57 -0700

    Configured production.rb to precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  RAILS_ENV=production rake assets:precompile
 end
